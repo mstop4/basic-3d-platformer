44a21cef1e4d67369b7ca9095f6be73687e71a17
M.S.T.O.P <mstop4@gmail.com>
2018-04-19 22:08:05 -0400

    added player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 8f;
+	public float jumpSpeed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hAxis = Input.GetAxis ("Horizontal");
+		print ("Horizontal axis");
+		print (hAxis);
+
+		float vAxis = Input.GetAxis ("Vertical");
+		print ("Vertical axis");
+		print (vAxis);
+	}
+}
