08aac51e8b4789de6242f864dc68ff3173b11fa1
M.S.T.O.P <mstop4@gmail.com>
2018-04-19 21:58:33 -0400

    fixed coins

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	public float rotationSpeed = 100f;
+
+	// Update is called once per frame
+	void Update () {
+		float angle = rotationSpeed * Time.deltaTime;
+		transform.Rotate (Vector3.up * angle, Space.World);
+	}
+}
