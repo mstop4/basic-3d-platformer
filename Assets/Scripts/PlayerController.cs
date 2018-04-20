3df174085b37b5d88f92e9011abaac67a1dc8646
M.S.T.O.P <mstop4@gmail.com>
2018-04-19 22:14:20 -0400

    added player walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,22 @@ public class PlayerController : MonoBehaviour {
 	public float walkSpeed = 8f;
 	public float jumpSpeed = 7f;
 
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float distance = walkSpeed * Time.deltaTime;
 		float hAxis = Input.GetAxis ("Horizontal");
-		print ("Horizontal axis");
-		print (hAxis);
-
 		float vAxis = Input.GetAxis ("Vertical");
-		print ("Vertical axis");
-		print (vAxis);
+
+		Vector3 movement = new Vector3 (hAxis * distance, 0f, vAxis * distance);
+		Vector3 curPosition = transform.position;
+		Vector3 newPosition = curPosition + movement;
+		rb.MovePosition (newPosition);
 	}
 }
